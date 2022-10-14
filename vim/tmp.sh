d35f8165249da2a96a0137a10dd927e8a900dca1
dermot <dkil1972@gmail.com>
2022-10-14 12:40:11 +0100

    removing submodules

---
+++ vim/tmp.sh
@@ -0,0 +1,8 @@
+git rm --cached pack/plugins/start/ctrlp && \
+git rm --cached pack/plugins/start/omnisharp-roslyn && \
+git rm --cached pack/plugins/start/vim-LanguageTool && \
+git rm --cached pack/plugins/start/vim-colors-solarized && \
+git rm --cached pack/plugins/start/vim-jsx-pretty && \
+git rm --cached pack/plugins/start/vim-multiple-cursors
+
+
