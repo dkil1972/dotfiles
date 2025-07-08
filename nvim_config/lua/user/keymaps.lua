b97aaca2aaf166cae32d8825e193f7bffba30e34
Dermot <dermot.kilroy+github@invoko.co.uk>
2025-07-08 22:14:04 +0100

    added a key binding for %%

--- nvim_config/lua/user/keymaps.lua
+++ nvim_config/lua/user/keymaps.lua
@@ -4,7 +4,7 @@ local keymap = vim.keymap
 
 -- reload all lua files and neovim config
 vim.keymap.set("n", "<leader><leader>r", function()
-  for name,_ in pairs(package.loaded) do
+  for name, _ in pairs(package.loaded) do
     if name:match("^user") then
       package.loaded[name] = nil
     end
@@ -13,3 +13,12 @@ vim.keymap.set("n", "<leader><leader>r", function()
   print("🔁 Config reloaded")
 end, { desc = "Reload Neovim config" })
 
+-- Expand %% to current buffer's directory in command-line mode
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[
+      cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h') . '/' : '%%'
+    ]])
+  end,
+})
