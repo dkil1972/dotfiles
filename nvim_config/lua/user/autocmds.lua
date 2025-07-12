fa76714c507ec3ae8c1fe2d12f0ecb74913e4a2e
Dermot <dermot.kilroy+github@invoko.co.uk>
2025-07-12 06:37:33 +0100

    now autoformatting jsx/tsx files

--- nvim_config/lua/user/autocmds.lua
+++ nvim_config/lua/user/autocmds.lua
@@ -1,5 +1,14 @@
+-- Format on save for Lua files
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.lua",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
+-- Format on save for JavaScript/TypeScript files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json" },
   callback = function()
     vim.lsp.buf.format({ async = false })
   end,
